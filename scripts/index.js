01af7ea8a3b55c817e508a2b1ace6654c3fc1e82
Joshua Wolfe <joshuamwolfe@gmail>
2020-06-18 12:06:46 -0400

    added prevent default and upload picture button

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,5 @@
-addEventListener('click', function(e){
-  let input = document.getElementsByTagName("input");
-  let currentClick = e.target;
-  if (input == currentClick ) {
-    input.value = '';
-  }
+addEventListener('submit', function(e){
+  e.preventDefault();
 
 })
 
