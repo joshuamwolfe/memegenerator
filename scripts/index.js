d77778f1e4a7040faa0b2deac512acfaa5c90cc5
Joshua Wolfe <joshuamwolfe@gmail>
2020-06-20 10:33:11 -0400

    asdf

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
-addEventListener('submit', function(e){
-  e.preventDefault();
-  let image = document.getElementById('image_file');
+addEventListener('submit', function(e) {
+	e.preventDefault();
+  let img = new Image();
+  let div = document.getElementById('picurl');
+   
+   
+  img.onload = function() {
+   
+    div.innerHTML += '<img src="'+img.src+'" />'; 
+   
+  };
+   
+   
+  img.src = 'image.jpg';
   
+  
+});
 
-})
 
