b87940fbf6e37559122cb760ef5c07f098d21214
Joshua Wolfe <joshuamwolfe@gmail>
2020-06-21 18:57:08 -0400

    added image upload ability

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,22 @@
 addEventListener('submit', function(e) {
 	e.preventDefault();
-  let img = new Image();
-  let div = document.getElementById('picurl');
+  // let img = new Image();
+  let UIurl = document.getElementById('picurl');
+  let test = UIurl.value; // rename variable
+  console.log(test);
+  let img = document.createElement('img');
+  img.setAttribute('src', test);
+  // append to the document with set attribute using said variable
+  let memeLocation = document.getElementById('location');
+  memeLocation.appendChild(img);
+  // img.onload = function() {
+   
+  //   div.innerHTML += '<img src="'+img.src+'" />'; 
+   
+  // };
    
    
-  img.onload = function() {
-   
-    div.innerHTML += '<img src="'+img.src+'" />'; 
-   
-  };
-   
-   
-  img.src = 'image.jpg';
+  // img.src = 'image.jpg';
   
   
 });
